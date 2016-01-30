f6efef5993e469cb21c219f1eee548809376e9ce
SpiColorPendra <zongmingli@outlook.com>
2016-01-30 20:43:48 +0800

    添加正负无穷大，NaN非数，字符和字符串拼接，表达式自动提升发生错误

--- 基本数据类型/BaseDataTypeDemo.java
+++ 基本数据类型/BaseDataTypeDemo.java
@@ -1,6 +1,7 @@
 /**
 Java是强类型的语言, -> 每个变量和每个表达式都在编译的时期确定了类型 ，必须先声明后使用
 */
+import java.util.*;
 public class BaseDataTypeDemo{
 	public static void main(String[] args) {
 		/*
@@ -33,5 +34,23 @@ public class BaseDataTypeDemo{
 		// syso : long -2147483648~2147483647  ????? 按道理long应该是64位  
 		// 解决方法　：　
 		// 虽然定义的 long_min  long_max是long类型 但是运行的时候 -1 和 1 没有加上 long类型  默认只有32位 其他位数精度丢失
+		float f = Float.NEGATIVE_INFINITY;
+		double d = Double.NEGATIVE_INFINITY;
+		//特殊的浮点数  :  NaN  ,  NaN不与任何数字相等,甚至和自己本身都不相等
+		//                 正无穷大 : POSITIVE_INFINITY
+		//                 负无穷大 : NEGATIVE_INFINITY
+		System.out.println(f == d);  // true
+		// Float 与 Double的正负无穷大是相等的
+
+		// 字符+字符串可以拼接
+		char c = 97;
+		String s = "java";
+		System.out.println(c + s);  //  ajava
+
+		// 表达类型自动提升
+		short sValue = -5;
+		sValue = sValue + 4;  // 企图把一个int类型赋值给short会发生错误
+		//BaseDataTypeDemo.java:52: 错误: 不兼容的类型: 从int转换到short可能会有损失
+
 	}
 }
