8f9ab3c529c53cb11989620e76e4336978e2b337
SpiColorPendra <zongmingli@outlook.com>
2016-01-30 16:01:30 +0800

    JavaDoc文档生成的示例

---
+++ Doc文档生成/DocDemo.java
@@ -0,0 +1,19 @@
+public class DocDemo{
+	public static void main(String[] args){
+		/**
+		@author SpiColorPendra
+		@version 1.0v
+
+
+		*/
+	}
+	/**
+	@param String s
+	@return void
+	@exception Exception
+	@see ..
+	*/
+	void print(String s){
+
+	}
+}
