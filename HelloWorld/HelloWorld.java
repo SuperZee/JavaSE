eb6b5eb1e83cb3c34eb96956d7d5c96b3ceda068
SpiColorPendra <zongmingli@outlook.com>
2016-01-30 16:02:25 +0800

    (时隔2个月的iOS来重构一下Java)添加了第一个Java程序

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+/*
+	EveryThing is beginning with that a person control the machine console 'helloworld'.  -SpiColorPendra
+*/
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld");
+	}
+}
