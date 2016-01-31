1b37a0ffc18616824541c147d0ec9d4ea919d1cb
SuperZee <zongmingli@outlook.com>
2016-02-01 01:10:52 +0800

    添加数组及其陷阱

---
+++ 数组及其陷阱/ArrayDemo.java
@@ -0,0 +1,27 @@
+public class ArrayDemo{
+	public static void main(String[] args){
+		// 定义两个数组
+		int[] a = {1,3,5,7};
+		int[] b = {2,4,6};
+		// 获取数组长度
+		System.out.println(a.length + "," + b.length);
+		// 遍历数组
+		for (int arr : a) {
+			System.out.println(arr);
+		}
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
